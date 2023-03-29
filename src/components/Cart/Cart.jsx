87520913f8059b7ca219d019762219fd679a51de
naimultusher <theislam720@gmail.com>
2023-03-29 22:41:06 +0600

    cart calculation added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,14 +5,25 @@ import './Cart.css'
 
 const Cart = (props) => {
     const { cart } = props;
+
+    let total = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = total*7/100;
+    const grandTotal = total + totalShipping + tax;
+    const finalTotal = grandTotal.toFixed(2);
+
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price:</p>
-            <p>Total Shipping Charge:</p>
-            <p>Tax:</p>
-            <h6 className='grand-total'>Grand Total:</h6>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h6 className='grand-total'>Grand Total: ${finalTotal}</h6>
             <button className='clear-cart'>
                 Clear Cart 
                 <FontAwesomeIcon icon={faTrash} />
