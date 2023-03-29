2b79f9096fa9096b93c4eeeb31f22942fcc67e85
naimultusher <theislam720@gmail.com>
2023-03-29 19:47:25 +0600

    cart component added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import './Cart.css'
 
-const Cart = () => {
+const Cart = (props) => {
+    const { cart } = props;
     return (
-        <div>
-            
+        <div className='cart'>
+            <h3>Order Summary</h3>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price:</p>
+            <p>Total Shipping Charge:</p>
+            <p>Tax:</p>
+            <h6 className='grand-total'>Grand Total:</h6>
+            <button className='clear-cart'>
+                Clear Cart 
+                <FontAwesomeIcon icon={faTrash} />
+            </button>
+            <button className='review-order'>
+                Review Order
+                <FontAwesomeIcon icon={faArrowRight} />
+            </button>
         </div>
     );
 };
